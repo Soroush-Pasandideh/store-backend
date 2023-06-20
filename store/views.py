4f98ed9bef8f424238645286b7dd93a7fdfc4aa0
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-06-21 02:47:13 +0330

    adding search feature to ProductViewSet class for searching among products base on title and description fields.

--- store/views.py
+++ store/views.py
@@ -1,9 +1,10 @@
 from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -18,8 +19,9 @@ from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
 
     def get_serializer_context(self):
         return {'request': self.request}
