8aa385b998378c0858a5deb6743d2326509919c4
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-06-21 03:46:38 +0330

    adding pagination feature to ProductViewSet class. in next commit I will change pagination how it works for all models, not only products.

--- store/views.py
+++ store/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -23,6 +24,7 @@ class ProductViewSet(ModelViewSet):
     filterset_class = ProductFilter
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
+    pagination_class = PageNumberPagination
 
     def get_serializer_context(self):
         return {'request': self.request}
